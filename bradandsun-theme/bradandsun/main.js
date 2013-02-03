ff2554bd8612bb60fec22fd179617ae03bd00822
Brad Ito <kosmikfox@gmail.com>
2013-02-02 22:36:51 -0800

    styling

--- bradandsun-theme/bradandsun/main.js
+++ bradandsun-theme/bradandsun/main.js
@@ -1,7 +1,11 @@
 
 (function($) {
 	$(document).ready(function() {
-		alert('test');
+		/*
+		 * Do some cross-browser styling
+		 */
+		$('.front-page-name-box').css('border-radius', '15px');
+		$('.links a').css('border-radius', '10px');
 	});
 })(jQuery);
 
