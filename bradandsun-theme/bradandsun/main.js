c8b277775a53099ff3b41ac80e67c79952089aa2
Brad Ito <kosmikfox@gmail.com>
2013-02-02 23:02:19 -0800

    more menu styling

--- bradandsun-theme/bradandsun/main.js
+++ bradandsun-theme/bradandsun/main.js
@@ -15,15 +15,15 @@
 		/*
 		 * Do some cross-browser styling
 		 */
-		$('.page-container').css('border-radius', '5px');
+		$('.page-container').css('border-radius', '10px');
 		$('.front-page-name-box').css('border-radius', '15px');
-		$('.links a').css('border-radius', '10px');
+		$('.links a').css('border-radius', '4px');
 		
 		/*
 		 * Load fonts after the initial page load
 		 * to make things appear sooner to the user
 		 */
-		loadCss('http://fonts.googleapis.com/css?family=Sacrament');
+		loadCss('http://fonts.googleapis.com/css?family=Sacramento');
 		loadCss('http://fonts.googleapis.com/css?family=Happy+Monkey');
 	});
 })(jQuery);
