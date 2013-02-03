1d2325bd06ba9f6cb3ff05592db6c56fdb111282
Brad Ito <kosmikfox@gmail.com>
2013-02-02 22:50:19 -0800

    changing how fonts are loaded

--- bradandsun-theme/bradandsun/main.js
+++ bradandsun-theme/bradandsun/main.js
@@ -6,6 +6,28 @@
 		 */
 		$('.front-page-name-box').css('border-radius', '15px');
 		$('.links a').css('border-radius', '10px');
+		
+		/*
+		 * Load fonts after the initial page load
+		 * to make things appear sooner to the user
+		 */
+		$('<link>')
+		.attr({
+			'rel':'stylesheet'
+			,'type':'text/css'
+			,'href':'http://fonts.googleapis.com/css?family=Sacramento'
+		})
+		.appendTo('head')
+		;
+		
+		$('<link>')
+		.attr({
+			'rel':'stylesheet'
+			,'type':'text/css'
+			,'href':'http://fonts.googleapis.com/css?family=Happy+Monkey'
+		})
+		.appendTo('head')
+		;
 	});
 })(jQuery);
 
--- bradandsun-theme/bradandsun/template.php
+++ bradandsun-theme/bradandsun/template.php
@@ -1,7 +1,6 @@
 <?php
 		
-drupal_add_css('http://fonts.googleapis.com/css?family=Sacramento');
-drupal_add_css('http://fonts.googleapis.com/css?family=Happy+Monkey');
+
 
 
 ?>
