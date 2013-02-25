cca8d7926e39856b8292ff910c53a34df9d1ad21
Brad Ito <kosmikfox@gmail.com>
2013-02-25 00:38:48 -0800

    removing display of page title

--- bradandsun-theme/bradandsun/page.tpl.php
+++ bradandsun-theme/bradandsun/page.tpl.php
@@ -126,11 +126,6 @@
   <?php print $messages; ?>
   
   <div id="main-wrapper"><div id="main" class="clearfix">
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h1 class="title" id="page-title"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
     
     <?php print render($page['content']); ?>
   </div></div> <!-- /#main, /#main-wrapper -->
